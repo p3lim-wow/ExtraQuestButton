770a32b3839963f304ea0accdb715f88b8bd393b
Adrian L Lange <mail@p3lim.net>
2021-07-18 14:59:10 +0200

    Add deDE translations provided by @Nulgar

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -1,10 +1,10 @@
 local L = select(2, ...).L('deDE')
 
 -- Config
--- L['Only show for tracked quests'] = ''
--- L['Only show for quests in current zone'] = ''
--- L['Distance in yards how far away the quest can be'] = ''
--- L['Prefer to show button for world quests'] = ''
+L['Only show for tracked quests'] = 'Nur für verfolgte Quests anzeigen'
+L['Only show for quests in current zone'] = 'Nur für Quests in der aktuellen Zone anzeigen'
+L['Distance in yards how far away the quest can be'] = 'Maximaler Abstand zur Quest in m'
+L['Prefer to show button for world quests'] = 'Button für Weltquests bevorzugt anzeigen'
 -- L['Up'] = ''
 -- L['Down'] = ''
 -- L['Both'] = ''
