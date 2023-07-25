723e49c39f888d3fda7e87058e1b5ed86c2314e5
Adrian L Lange <git@p3lim.net>
2023-07-25 20:01:09 +0200

    Add zhTW translations (@BlueNightSky)
    Fixes #80

--- locale/zhTW.lua
+++ locale/zhTW.lua
@@ -1,9 +1,9 @@
 local L = select(2, ...).L('zhTW') -- luacheck: ignore
 
--- L['Button scale'] = '' -- MISSING TRANSLATION
--- L['Artwork opacity'] = '' -- MISSING TRANSLATION
--- L['Artwork style'] = '' -- MISSING TRANSLATION
--- L['Hide cooldown text'] = '' -- MISSING TRANSLATION
--- L['Only show for tracked quests'] = '' -- MISSING TRANSLATION
--- L['Only show for quests in current zone'] = '' -- MISSING TRANSLATION
--- L['Tracking distance'] = '' -- MISSING TRANSLATION
+L['Button scale'] = '按鈕大小'
+L['Artwork opacity'] = '美術透明度'
+L['Artwork style'] = '美術樣式'
+L['Hide cooldown text'] = '隱藏冷卻文字'
+L['Only show for tracked quests'] = '只顯示已追蹤任務'
+L['Only show for quests in current zone'] = '只顯示當前區域任務'
+L['Tracking distance'] = '追蹤距離'
