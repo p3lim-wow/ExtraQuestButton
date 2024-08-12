bc2fbb605788d64942547ced205733d6b1f7814a
Adrian L Lange <git@p3lim.net>
2024-08-12 21:14:57 +0200

    Add deDE translations
    Thanks @Marakuja
    
    Closes #95

--- locale/deDE.lua
+++ locale/deDE.lua
@@ -1,9 +1,9 @@
-local L = select(2, ...).L('deDE') -- luacheck: ignore
+local L = select(2, ...).L('deDE')
 
--- L['Button scale'] = '' -- MISSING TRANSLATION
--- L['Artwork opacity'] = '' -- MISSING TRANSLATION
--- L['Artwork style'] = '' -- MISSING TRANSLATION
--- L['Hide cooldown text'] = '' -- MISSING TRANSLATION
--- L['Only show for tracked quests'] = '' -- MISSING TRANSLATION
--- L['Only show for quests in current zone'] = '' -- MISSING TRANSLATION
--- L['Tracking distance'] = '' -- MISSING TRANSLATION
+L['Button scale'] = 'Skalierung'
+L['Artwork opacity'] = 'Transparenz der Grafik'
+L['Artwork style'] = 'Gestaltung der Grafik'
+L['Hide cooldown text'] = 'Abklingzeitsanzeige verstecken'
+L['Only show for tracked quests'] = 'Nur für verfolgte Quests anzeigen'
+L['Only show for quests in current zone'] = 'Nur für Quests in der aktuellen Zone anzeigen'
+L['Tracking distance'] = 'Verfolgungsdistanz'
