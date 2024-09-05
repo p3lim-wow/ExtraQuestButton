9532f3bb831fe35b8d859d1646d75dca7fb52a09
Adrian L Lange <git@p3lim.net>
2024-09-05 17:46:54 +0200

    Add zhCN translations
    Fixes #97

--- locale/zhCN.lua
+++ locale/zhCN.lua
@@ -1,9 +1,9 @@
-local L = select(2, ...).L('zhCN') -- luacheck: ignore
+local L = select(2, ...).L('zhCN')
 
--- L['Button scale'] = '' -- MISSING TRANSLATION
--- L['Artwork opacity'] = '' -- MISSING TRANSLATION
--- L['Artwork style'] = '' -- MISSING TRANSLATION
--- L['Hide cooldown text'] = '' -- MISSING TRANSLATION
--- L['Only show for tracked quests'] = '' -- MISSING TRANSLATION
--- L['Only show for quests in current zone'] = '' -- MISSING TRANSLATION
--- L['Tracking distance'] = '' -- MISSING TRANSLATION
+L['Button scale'] = '按钮缩放'
+L['Artwork opacity'] = '外观透明度'
+L['Artwork style'] = '外观样式'
+L['Hide cooldown text'] = '隐藏冷却文字'
+L['Only show for tracked quests'] = '只显示已追踪的任务'
+L['Only show for quests in current zone'] = '只显示所在地区的任务'
+L['Tracking distance'] = '追踪距离'
