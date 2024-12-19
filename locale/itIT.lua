bf17dfe5c89574e16cee288296aded53d3530b3d
Adrian L Lange <git@p3lim.net>
2024-12-19 04:08:46 +0100

    Remove untranslated strings

--- locale/itIT.lua
+++ locale/itIT.lua
@@ -1,9 +1,1 @@
 local L = select(2, ...).L('itIT') -- luacheck: ignore
-
--- L['Button scale'] = '' -- MISSING TRANSLATION
--- L['Artwork opacity'] = '' -- MISSING TRANSLATION
--- L['Artwork style'] = '' -- MISSING TRANSLATION
--- L['Hide cooldown text'] = '' -- MISSING TRANSLATION
--- L['Only show for tracked quests'] = '' -- MISSING TRANSLATION
--- L['Only show for quests in current zone'] = '' -- MISSING TRANSLATION
--- L['Tracking distance'] = '' -- MISSING TRANSLATION
