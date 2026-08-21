145de5963f857bf788aaf12f5754ea4db759213b
Adrian L Lange <git@p3lim.net>
2026-08-21 22:28:02 +0200

    Revamp quest item logic

--- utils.lua
+++ utils.lua
@@ -1,218 +1,247 @@
 local _, addon = ...
-local data = addon.data
 
+local data = addon.data
 local HBD = LibStub('HereBeDragons-2.0')
 local sqrt = math.sqrt
 
-local function GetDistanceSqToPoint(mapID, x, y)
-	local playerX, playerY, playerMapID = HBD:GetPlayerZonePosition()
-	return (HBD:GetZoneDistance(playerMapID, playerX, playerY, mapID, x, y))
+local questItemsByPriority = {}
+local processedQuests = {}
+
+local function getQuestDistance(questID, maxDistanceYd, mapID, x, y)
+	local distanceSq = C_QuestLog.GetDistanceSqToQuest(questID)
+	if distanceSq then
+		local distanceYd = sqrt(distanceSq)
+		if distanceYd <= maxDistanceYd then
+			return distanceYd
+		end
+	end
+
+	local area = data.accurateQuestAreas[questID]
+	if area then
+		local distanceSqToPoint = HBD:GetZoneDistance(mapID, x, y, area[1], area[2], area[3])
+		if distanceSqToPoint then
+			return sqrt(distanceSqToPoint)
+		end
+	end
+
+	area = data.inaccurateQuestAreas[questID]
+	if area then
+		local areaType = type(area)
+		if areaType == 'boolean' then
+			return maxDistanceYd - 1
+		elseif areaType == 'number' then
+			if area == mapID then
+				return maxDistanceYd - 2
+			end
+		elseif areaType == 'table' then
+			for _, questAreaMapID in next, area do
+				if questAreaMapID == mapID then
+					return maxDistanceYd - 2
+				end
+			end
+		end
+	end
 end
 
-local function GetQuestDistanceWithItem(questID, maxDistanceYd)
-	local questLogIndex = C_QuestLog.GetLogIndexForQuestID(questID)
-	if not questLogIndex then
-		return
+local function isQuestOnCurrentMap(questID, mapID)
+	if C_QuestLog.IsOnMap(questID) then
+		return true
 	end
 
-	local itemLink, _, _, showWhenComplete = GetQuestLogSpecialItemInfo(questLogIndex)
-	if not itemLink then
-		local fallbackItemID = data.questItems[questID]
-		if fallbackItemID == 202247 then
-			-- extra handling for technoscrying quests
-			if C_UnitAuras.GetPlayerAuraBySpellID(409668) or C_UnitAuras.GetPlayerAuraBySpellID(414539) then
-				fallbackItemID = nil
-			end
-		end
-		if fallbackItemID then
-			if type(fallbackItemID) == 'table' then
-				for _, itemID in next, fallbackItemID do
-					local link = addon:GetItemLinkFromID(itemID)
-					if C_Item.GetItemCount(link) > 0 then
-						itemLink = link
-						break
-					end
+	local area = data.accurateQuestAreas[questID]
+	if area and area[1] == mapID then
+		return true
+	end
+
+	area = data.inaccurateQuestAreas[questID]
+	if area then
+		local areaType = type(area)
+		if areaType == 'boolean' then
+			return true
+		elseif areaType == 'number' then
+			return area == mapID
+		elseif areaType == 'table' then
+			for _, questAreaMapID in next, area do
+				if questAreaMapID == mapID then
+					return true
 				end
-			else
-				itemLink = addon:GetItemLinkFromID(fallbackItemID)
 			end
 		end
 	end
 
-	if not itemLink then
-		return
-	end
+	return false
+end
 
-	local itemID = GetItemInfoFromHyperlink(itemLink)
-	if C_QuestLog.IsComplete(questID) then
-		local completeItemZone = data.completeItems[itemID]
-		if not showWhenComplete and not completeItemZone then
+local function getValidQuestItem(questID, mapID, itemLink, itemID)
+	local showWhenComplete, _
+	if not itemLink then
+		local questLogIndex = C_QuestLog.GetLogIndexForQuestID(questID)
+		if not questLogIndex then
 			return
 		end
 
-		if completeItemZone and type(completeItemZone) == 'number' then
-			if HBD:GetPlayerZone() ~= completeItemZone then
-				return
-			end
+		itemLink, _, _, showWhenComplete = GetQuestLogSpecialItemInfo(questLogIndex)
+		if not itemLink then
+			return
 		end
 
+		itemID = GetItemInfoFromHyperlink(itemLink)
+	end
+
+	if C_QuestLog.IsComplete(questID) then
 		local noCompleteItem = data.noCompleteItems[itemID]
 		if noCompleteItem then
-			if type(noCompleteItem) == 'number' then
-				itemLink = addon:GetItemLinkFromID(noCompleteItem)
-				itemID = noCompleteItem
-			else
+			if type(noCompleteItem) ~= 'number' then
 				return
 			end
+
+			itemID = noCompleteItem
+			itemLink = addon:GetItemLinkFromID(itemID)
+		end
+
+		local completeItem = data.completeItems[itemID]
+		if not showWhenComplete and not completeItem then
+			return
+		end
+
+		if type(completeItem) == 'number' and completeItem ~= mapID then
+			return
 		end
 	end
 
 	if C_Item.GetItemCount(itemLink) == 0 then
-		-- no point showing items we don't have
 		return
 	end
 
 	if data.itemBlacklist[itemID] then
-		-- don't show items we specifically want to ignore
 		return
 	end
 
-	local distanceSq = C_QuestLog.GetDistanceSqToQuest(questID)
-	 -- the square root of distanceSq is in yards, much easier to work with
-	local distanceYd = distanceSq and sqrt(distanceSq)
-	if distanceYd and distanceYd <= maxDistanceYd then
-		return distanceYd, itemLink
+	return itemLink, itemID
+end
+
+local function processQuestItem(questID, distance, mapID, ...)
+	local itemLink, itemID = getValidQuestItem(questID, mapID, ...)
+	if not itemLink then
+		return
 	end
 
-	local accurateQuestAreaData = data.accurateQuestAreas[questID]
-	if accurateQuestAreaData then
-		local distanceSqToPoint = GetDistanceSqToPoint(accurateQuestAreaData[1], accurateQuestAreaData[2], accurateQuestAreaData[3])
-		if distanceSqToPoint then
-			return sqrt(distanceSqToPoint), itemLink
-		end
+	local priority = data.priorityItems[itemID]
+	if not distance and priority == nil then
+		return
 	end
 
-	local questMapID = data.inaccurateQuestAreas[questID]
-	if questMapID then
-		if type(questMapID) == 'boolean' then
-			return maxDistanceYd - 1, itemLink
-		elseif type(questMapID) == 'number' then
-			if questMapID == HBD:GetPlayerZone() then
-				return maxDistanceYd - 2, itemLink
-			end
-		elseif type(questMapID) == 'table' then
-			local playerMapID = HBD:GetPlayerZone()
-			for _, mapID in next, questMapID do
-				if mapID == playerMapID then
-					return maxDistanceYd - 2, itemLink
-				end
-			end
-		end
+	if not priority then
+		priority = 0
+	end
+
+	local current = questItemsByPriority[priority]
+	if not current then
+		questItemsByPriority[priority] = {
+			itemLink,
+			distance,
+		}
+	elseif distance and (not current[2] or distance < current[2]) then
+		current[1] = itemLink
+		current[2] = distance
 	end
 end
 
-local function IsQuestOnMapCurrentMap(questID)
-	local isOnMap = C_QuestLog.IsOnMap(questID)
-	if not isOnMap then
-		local accurateQuestAreaInfo = data.accurateQuestAreas[questID]
-		if accurateQuestAreaInfo then
-			isOnMap = accurateQuestAreaInfo[1] == HBD:GetPlayerZone()
+local function processQuestItemsFromData(questID, distance, mapID)
+	local dataItem = data.questItems[questID]
+	if not dataItem then
+		return
+	end
+
+	if dataItem == 202247 and not C_Secrets.ShouldAurasBeSecret() then
+		-- special handling for technoscrying quests
+		if C_UnitAuras.GetPlayerAuraBySpellID(409668) or C_UnitAuras.GetPlayerAuraBySpellID(414539) then
+			return
 		end
 	end
 
-	if not isOnMap then
-		local inaccurateQuestAreaInfo = data.inaccurateQuestAreas[questID]
-		if inaccurateQuestAreaInfo then
-			if type(inaccurateQuestAreaInfo) == 'boolean' then
-				isOnMap = true
-			elseif type(inaccurateQuestAreaInfo) == 'table' then
-				local playerMapID = HBD:GetPlayerZone()
-				for _, mapID in next, inaccurateQuestAreaInfo do
-					if mapID == playerMapID then
-						isOnMap = true
-						break
-					end
-				end
-			else
-				isOnMap = inaccurateQuestAreaInfo == HBD:GetPlayerZone()
+	if type(dataItem) == 'table' then
+		for _, itemID in next, dataItem do
+			local itemLink = addon:GetItemLinkFromID(itemID)
+			if itemLink then
+				processQuestItem(questID, distance, mapID, itemLink, itemID)
 			end
 		end
-	end
-
-	return isOnMap
-end
-
-local function sortByClosestDistance(a, b)
-	return a[2] < b[2]
-end
-
-local function GetItemPriority(itemLink)
-	local itemID = GetItemInfoFromHyperlink(itemLink or '') or -1
-	return data.priorityItems[itemID]
-end
-
-local uniqueItems = {}
-local prioritizedItemLinks = {}
-local function addPrioritizedItem(questID, maxDistanceYd)
-	local distance, itemLink = GetQuestDistanceWithItem(questID, maxDistanceYd)
-	local priorityIndex = GetItemPriority(itemLink)
-	if distance or priorityIndex then
-		if not priorityIndex then
-			priorityIndex = 0
-		end
-
-		if not prioritizedItemLinks[priorityIndex] then
-			prioritizedItemLinks[priorityIndex] = {}
-		end
-
-		if not uniqueItems[itemLink] then
-			table.insert(prioritizedItemLinks[priorityIndex], {itemLink, distance})
-			uniqueItems[itemLink] = true
+	elseif dataItem then
+		local itemLink = addon:GetItemLinkFromID(dataItem)
+		if itemLink then
+			processQuestItem(questID, distance, mapID, itemLink, dataItem)
 		end
 	end
+end
+
+local function processQuest(questID, maxDistanceYd, zoneOnly, mapID, x, y)
+	if not questID then
+		return
+	end
+
+	if processedQuests[questID] then
+		return
+	end
+
+	processedQuests[questID] = true
+
+	if zoneOnly and not isQuestOnCurrentMap(questID, mapID) then
+		return
+	end
+
+	local distance = getQuestDistance(questID, maxDistanceYd, mapID, x, y)
+	processQuestItem(questID, distance, mapID)
+	processQuestItemsFromData(questID, distance, mapID)
 end
 
 function addon:GetClosestQuestItem(maxDistanceYd, zoneOnly, trackingOnly)
-	table.wipe(prioritizedItemLinks)
-	table.wipe(uniqueItems)
+	table.wipe(processedQuests)
+	table.wipe(questItemsByPriority)
 
+	-- query player position once and pass it through to avoid querying it for every quest
+	local mapID = HBD:GetPlayerZone()
+	local x, y = HBD:GetPlayerZonePosition()
+
+	-- process supertracked quests
 	for index = 1, C_QuestLog.GetNumWorldQuestWatches() do
-		-- this only tracks supertracked worldquests,
-		-- e.g. stuff the player has shift-clicked on the map
 		local questID = C_QuestLog.GetQuestIDForWorldQuestWatchIndex(index)
-		if questID and (not zoneOnly or IsQuestOnMapCurrentMap(questID)) then
-			addPrioritizedItem(questID, maxDistanceYd)
-		end
+		processQuest(questID, maxDistanceYd, zoneOnly, mapID, x, y)
 	end
 
+	-- process tracked quests
 	for index = 1, C_QuestLog.GetNumQuestWatches() do
 		local questID = C_QuestLog.GetQuestIDForQuestWatchIndex(index)
-		if questID and QuestHasPOIInfo(questID) and (not zoneOnly or IsQuestOnMapCurrentMap(questID)) then
-			addPrioritizedItem(questID, maxDistanceYd)
+		if questID and QuestHasPOIInfo(questID) then
+			processQuest(questID, maxDistanceYd, zoneOnly, mapID, x, y)
 		end
 	end
 
+	-- process world quests / bonus objectives / quest log
 	for index = 1, C_QuestLog.GetNumQuestLogEntries() do
 		local info = C_QuestLog.GetInfo(index)
 		if info and not info.isHeader and info.hasLocalPOI then
 			local questID = info.questID
-			if C_QuestLog.IsWorldQuest(questID) or info.questClassification == Enum.QuestClassification.BonusObjective or (trackingOnly and not info.isHidden) then
-				if not zoneOnly or IsQuestOnMapCurrentMap(questID) then
-					addPrioritizedItem(questID, maxDistanceYd)
-				end
+			if
+				C_QuestLog.IsWorldQuest(questID) or
+				info.questClassification == Enum.QuestClassification.BonusObjective or
+				(trackingOnly and not info.isHidden)
+			then
+				processQuest(questID, maxDistanceYd, zoneOnly, mapID, x, y)
 			end
 		end
 	end
 
-	for _, items in next, prioritizedItemLinks do
-		if #items > 0 then
-			if #items > 1 then
-				table.sort(items, sortByClosestDistance)
-			end
+	-- after processing all quests find the highest prioritized quest item, if any
+	local highestPriority
+	local questItem
 
-			for _, itemInfo in next, items do
-				return itemInfo[1]
-			end
+	for priority, info in next, questItemsByPriority do
+		if not highestPriority or priority > highestPriority then
+			highestPriority = priority
+			questItem = info[1]
 		end
 	end
+
+	return questItem
 end
